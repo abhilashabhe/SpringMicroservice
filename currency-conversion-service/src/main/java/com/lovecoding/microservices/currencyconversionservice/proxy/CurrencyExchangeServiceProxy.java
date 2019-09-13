e373ece942e37fadfd65f2a12013e8960b40608e
Abhilash Reddy <abhilash.abhe@gmail.com>
2019-09-13 18:26:21 -0400

    currency conversion service

---
+++ currency-conversion-service/src/main/java/com/lovecoding/microservices/currencyconversionservice/proxy/CurrencyExchangeServiceProxy.java
@@ -0,0 +1,19 @@
+package com.lovecoding.microservices.currencyconversionservice.proxy;
+
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.lovecoding.microservices.currencyconversionservice.Bean.CurrencyConversionBean;
+
+
+//@FeignClient(name="currency-exchange-service")
+@FeignClient(name="netflix-zuul-api-server") //connecting to zuul api server instead  exchange server
+@RibbonClient(name="currency-exchange-service")
+public interface CurrencyExchangeServiceProxy {
+
+	//@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	@GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
+	public CurrencyConversionBean retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
+}
